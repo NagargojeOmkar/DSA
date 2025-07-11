efb9a81cb5ba181a8de5c04e6cbdc75d495ecb76
NagargojeOmkar <Konomkar@gmail.com>
2025-07-11 23:34:07 +0530

    Added only desired folders without submodule issue

---
+++ Python/Conditional_Statement/if_else.py
@@ -0,0 +1,12 @@
+num1=int(input("Enter your First number = "))
+num2=int(input("Enter your Second Number = "))
+
+if num1>=num2:
+    print("Num1" , num1)
+else:
+    print("Num2" , num2)
+
+
+
+
+
---
+++ Python/python_basics/Variable.py
@@ -0,0 +1,23 @@
+# x=10
+# y=16
+# z=x+y
+# print(x)
+# print(y)
+# print(z)
+
+
+name= "raj"
+age = 20
+surname = "DK"
+
+# print("my name is" , name)
+# print ("my surname is ", surname)
+# print("My age is ", age)
+
+
+"""METHOD F STRING"""
+
+
+print(f"my name is {name}")
+print(f"my age is {age}")
+print(f"my surname is {surname}")
