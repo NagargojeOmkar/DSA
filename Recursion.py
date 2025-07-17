572c4ae5a1d49a95f3567f22ce0ffd58d620d601
NagargojeOmkar <Konomkar@gmail.com>
2025-07-17 23:48:29 +0530

    DSA daily Practise

---
+++ Recursion.py
@@ -0,0 +1,42 @@
+# # """1 to N printing without using for while loop"""
+
+
+# # def func(i, n):
+# #     if i > n:
+# #         return
+# #     print(i)
+# #     func(i + 1, n)
+
+
+# # func(1, 5)
+
+
+# """Functional recursion"""
+
+
+# def func(n):
+
+#     if n == 1:
+#         return 1
+#     return n + func(n - 1)
+
+#     print(x)
+
+
+# func(10)
+
+
+"""Factorial of any numnber"""
+
+# Num = int(input("Entter any number = "))
+
+
+# def fun(n):
+#     if n == 1:
+#         return 1
+#     return n + fun(n - 1)
+
+
+# result = fun(Num)
+
+# print(result)
