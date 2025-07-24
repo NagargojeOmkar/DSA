4cf282816903bd53bcc9333e70f3ac37ddd4abea
NagargojeOmkar <Konomkar@gmail.com>
2025-07-25 00:07:27 +0530

    Updated Sorting.py

--- Sorting.py
+++ Sorting.py
@@ -89,55 +89,134 @@
 
 """ MERGE SOrt """
 
-""" 1. MERGING OF AN ARRAY """
+# """ 1. MERGING OF AN ARRAY """
 
 
-def merge_array(left, right):
-    i, j = 0, 0
-    n, m = len(left), len(right)
-    result = []
+# def merge_array(left, right):
+#     i, j = 0, 0
+#     n, m = len(left), len(right)
+#     result = []
 
-    while i < n and j < m:
+#     while i < n and j < m:
 
-        if left[i] <= right[j]:
-            result.append(left[i])
-            i += 1
-        else:
-            result.append(right[j])
-            j += 1
+#         if left[i] <= right[j]:
+#             result.append(left[i])
+#             i += 1
+#         else:
+#             result.append(right[j])
+#             j += 1
 
-    if i < n:
-        while i < n:
-            result.append(left[i])
-            i += 1
-    if j < m:
-        while j < m:
-            result.append(right[j])
-            j += 1
-    return result
-
-
-"""
-
-MERGE SORT OF AN ARRAY
-
-"""
+#     if i < n:
+#         while i < n:
+#             result.append(left[i])
+#             i += 1
+#     if j < m:
+#         while j < m:
+#             result.append(right[j])
+#             j += 1
+#     return result
 
 
-def merge_sort(arr):
-    if len(arr) <= 1:
-        return arr
-    mid = len(arr) // 2
-    left = arr[:mid]
-    right = arr[mid:]
+# """
 
-    left = merge_sort(left)
-    right = merge_sort(right)
+# MERGE SORT OF AN ARRAY
 
-    return merge_array(left, right)
+# """
 
 
-arr = [2, 5, 6, 8, 4, 6, 3, 5, 6, 8, 35, 6879, 545, 465]
+# def merge_sort(arr):
+#     if len(arr) <= 1:
+#         return arr
+#     mid = len(arr) // 2
+#     left = arr[:mid]
+#     right = arr[mid:]
 
-sorted = merge_sort(arr)
-print(sorted)
+#     left = merge_sort(left)
+#     right = merge_sort(right)
+
+#     return merge_array(left, right)
+
+
+# arr = [2, 5, 6, 8, 4, 6, 3, 5, 6, 8, 35, 6879, 545, 465]
+
+# sorted = merge_sort(arr)
+# print(sorted)
+
+
+""" Practise   """
+
+
+# def merge_array(left, right):
+#     i, j = 0, 0
+#     n, m = len(left), len(right)
+#     Result = []
+
+#     while i < n and j < m:
+#         if left[i] <= right[j]:
+#             Result.append(left[i])
+#             i += 1
+#         else:
+#             Result.append(right[j])
+#             j += 1
+
+#     while i < n:
+#         Result.append(left[i])
+#         i += 1
+
+#     while j < m:
+#         Result.append(right[j])
+#         j += 1
+#     return Result
+
+
+# def merge_sort(arr):
+#     if len(arr) <= 1:
+#         return arr
+#     mid = len(arr) // 2
+#     left_hand = arr[:mid]
+#     right_hand = arr[mid:]
+
+#     left = merge_sort(left_hand)
+#     right = merge_sort(right_hand)
+
+#     return merge_array(left, right)
+
+
+# arr = [8, 9, 6, 5, 7478, 2, 365, 4, 87, 96, 4, 123, 65, 789, 4, 146, 6]
+
+# sorted = merge_sort(arr)
+# print(sorted)
+
+""" QUICK SORT ALGORITHM """
+
+
+# nums = [4, 1, 2, 6, 45, 54634, 3, 23, 1, 124, 213, 256, 3, 7, 8]
+
+
+# def partiation(nums, low, high):
+#     pivot = nums[low]
+#     i = low
+#     j = high
+
+#     while i < j:
+#         while nums[i] <= pivot and i <= high - 1:
+#             i += 1
+#         while nums[j] > pivot and j >= low + 1:
+#             j -= 1
+
+#         if i < j:
+#             nums[i], nums[j] = nums[j], nums[i]
+#     nums[low], nums[j] = nums[j], nums[low]
+#     return j
+
+
+# def quickSort(nums, low, high):
+#     if low < high:
+#         p_ind = partiation(nums, low, high)
+#         quickSort(nums, low, p_ind - 1)
+#         quickSort(nums, p_ind + 1, high)
+
+
+# ans = quickSort(nums, 0, len(nums) - 1)
+
+# print(nums)
